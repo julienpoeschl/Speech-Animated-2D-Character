d07f7c22b86eec9b198879ba1bf5ea77761ad5c2
julienpoeschl <julienpoeschl@outlook.de>
2025-10-20 20:57:24 +0200

    updated path from media to assets

--- app/src/character_loader.py
+++ app/src/character_loader.py
@@ -12,15 +12,15 @@ def load_character_as_pixmap(face_type : FaceType) -> QPixmap:
     
     match face_type:
         case FaceType.Closed:
-            pixmap = QPixmap("app/media/character_1.png")
+            pixmap = QPixmap("app/assets/character_1.png")
         case FaceType.HalfOpen:
-            pixmap = QPixmap("app/media/character_5.png")
+            pixmap = QPixmap("app/assets/character_5.png")
         case FaceType.LeftOpen:
-            pixmap = QPixmap("app/media/character_2.png")
+            pixmap = QPixmap("app/assets/character_2.png")
         case FaceType.RightOpen:
-            pixmap = QPixmap("app/media/character_3.png")
+            pixmap = QPixmap("app/assets/character_3.png")
         case FaceType.Open:
-            pixmap = QPixmap("app/media/character_4.png")
+            pixmap = QPixmap("app/assets/character_4.png")
         case _:
             raise RuntimeError("ERROR: Selected face type isn't supported or doesn't exist.")
 
