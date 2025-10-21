35e48e0abcd31cab6a8a7e6182faf149ad9d2e4a
julienpoeschl <julienpoeschl@outlook.de>
2025-10-21 19:05:05 +0200

    refactored screen parameter

--- app/src/main.py
+++ app/src/main.py
@@ -2,12 +2,11 @@ from PyQt6.QtWidgets import QApplication
 import sys
 
 from .controller import Controller
-from .window import AppWindow
+from .gui.window import AppWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     controller = Controller()
-    screen = app.primaryScreen()
-    window = AppWindow(screen, controller)
+    window = AppWindow(app, controller)
     window.show()
     sys.exit(app.exec())
