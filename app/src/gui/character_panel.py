901ba86dfc5a8820d8f59a7614ba9d30f3cbc022
julienpoeschl <julienpoeschl@outlook.de>
2025-10-22 01:04:52 +0200

    docs

--- app/src/gui/character_panel.py
+++ app/src/gui/character_panel.py
@@ -1,31 +1,43 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PyQt6.QtCore import Qt
 
-from ..controller import Controller
+from ..controller import AppController
 from ..frame_loader import FrameLoader
 
 
 class CharacterPanel(QWidget):
-    def __init__(self, controller : Controller, parent=None):
+    """
+    QWidget that displays a character frame.
+    """
+
+    def __init__(self, controller : AppController, parent=None):
+        """
+        Args:
+            controller (AppController): Application controller as interface to functionality.
+        """
         super().__init__(parent)
 
-        self.controller = controller
         frame_loader = FrameLoader()
-        self.frame_loader = frame_loader
 
         layout = QVBoxLayout(self)
 
-        # Character image
+        # --- Character image ---
         self.character_label = QLabel()
         self.character_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.character_label)
 
-        pixmap = frame_loader.get_frame_pixmap(self.controller.curr_face_type)
+        pixmap = frame_loader.get_frame_pixmap(controller.curr_face_type)
         self.character_label.setPixmap(pixmap)
         self.character_label.setScaledContents(True)
 
+        self.controller = controller
+        self.frame_loader = frame_loader
+
 
     def update(self) -> None:
+        """
+        Get current character frame from frame loader and update the panel.
+        """
         curr_face_type = self.controller.evaluate_audio()
         pixmap = self.frame_loader.get_frame_pixmap(curr_face_type)
         self.character_label.setPixmap(pixmap)
