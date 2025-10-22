365234bb4ba58ee7a4b0ab82d2632e91db13e7ed
julienpoeschl <julienpoeschl@outlook.de>
2025-10-22 02:01:39 +0200

    refactor

--- app/src/gui/settings_panel.py
+++ app/src/gui/settings_panel.py
@@ -1,59 +1,59 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSlider, QComboBox, QHBoxLayout
 from PyQt6.QtCore import Qt
 
-from ..controller import Controller
+from ..controller import AppController
 
 class SettingsPanel(QWidget):
-    def __init__(self, controller : Controller, parent=None):
+    """
+    QWidget that contains interactable settings.
+    """
+
+    def __init__(self, controller : AppController, parent=None):
+        """
+        Args:
+            controller (AppController): Application controller as interface to functionality.
+        """
         super().__init__(parent)
 
         self.controller = controller
-        settings_layout = QVBoxLayout(self)
+        layout = QVBoxLayout(self)
 
-        # Example settings
         self.device_label = QLabel("Input Device:")
-        settings_layout.addWidget(self.device_label)
+        layout.addWidget(self.device_label)
 
+ 
         self.device_combo = QComboBox()
-        self.device_combo.addItems(controller.get_device_names())  # populate dynamically later
-        settings_layout.addWidget(self.device_combo)
+        self.device_combo.addItems(controller.get_device_names())
+        layout.addWidget(self.device_combo)
         default_index = controller.get_default_device_index()
         self.device_combo.setCurrentIndex(default_index)
         self.device_combo.currentIndexChanged.connect(controller.on_device_index_changed)
         controller.on_device_index_changed(default_index)
 
-        self.rms_threshold_label = QLabel("RMS Threshold:")
-        settings_layout.addWidget(self.rms_threshold_label)
-
-        self.rms_slider = QSlider(Qt.Orientation.Horizontal)
-        self.rms_slider.setMinimum(0)
-        self.rms_slider.setMaximum(10000)
-        self.rms_slider.setValue(1000)
-        settings_layout.addWidget(self.rms_slider)
 
         self.db_threshold_label = QLabel("Audio level (dB):")
-        settings_layout.addWidget(self.db_threshold_label)
+        layout.addWidget(self.db_threshold_label)
 
-        # --- Create slider ---
         self.db_volume_slider = QSlider(Qt.Orientation.Horizontal)
         self.db_volume_slider.setMinimum(-60)   # typical dB min
         self.db_volume_slider.setMaximum(0)     # max volume (0 dB)
         self.db_volume_slider.setValue(-60)
         #self.db_threshold_slider.setDisabled(True)  # makes it uninteractable
         self.db_volume_slider.valueChanged.connect(controller.set_db_volume_threshold)
-        settings_layout.addWidget(self.db_volume_slider)
-        # --- Style the slider ---
+        layout.addWidget(self.db_volume_slider)
+
         self.update_db_volume_slider(0.0)
 
         tick_layout = QHBoxLayout()
-        settings_layout.addLayout(tick_layout)
+        layout.addLayout(tick_layout)
 
-        for db in range(-60, 1, 10):  # -60, -50, ..., 0
+        for db in range(-60, 1, 10):
             lbl = QLabel(str(db))
             lbl.setAlignment(Qt.AlignmentFlag.AlignHCenter)
             tick_layout.addWidget(lbl)
 
-        settings_layout.addStretch()  # push items to top
+
+        layout.addStretch()  # push items to top
 
 
 
@@ -85,8 +85,7 @@ class SettingsPanel(QWidget):
         self.db_volume_slider.setStyleSheet(gradient)
 
     def update_volume(self):
-        # simulate dB value between -60 and 0
-        db = self.controller.db_volume
+        db = self.controller.db_volume_threshold
         #self.db_volume_slider.setValue(int(db))
 
         # convert dB range (-60..0) → (0..1)
